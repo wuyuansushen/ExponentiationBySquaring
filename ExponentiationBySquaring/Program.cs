01c1f318aeaae413d03a4329865a3a5445054e23
wuyuansushen <819070864@qq.com>
2021-03-19 19:05:31 +0800

    Add project files.

---
+++ ExponentiationBySquaring/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExponentiationBySquaring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input base value: ");
+            int Ivalue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input exponent: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+
+            int evenresult=1;
+            int oddResult = Ivalue;
+
+            while(exponent!=0)
+            {
+                if (exponent % 2 == 1)
+                    evenresult *= oddResult;//Collect rest odd exponent
+                else
+                { }
+                oddResult *= oddResult;
+                exponent /= 2;
+            }
+
+            Console.WriteLine($"output: {evenresult}");
+        }
+    }
+}
